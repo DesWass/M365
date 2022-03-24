b5ac09ad33df8b926498723a507581536072f399
Des Wass <des@thewassfamily.net>
2022-03-24 16:39:27 +1100

    Objects to SID

---
+++ M365-ConvertObjecttoSID.ps1
@@ -0,0 +1,33 @@
+$objID = Read-Host "What's the ObjectID: "
+function Convert-AzureAdObjectIdToSid {
+    <#
+    .SYNOPSIS
+    Convert an Azure AD Object ID to SID
+     
+    .DESCRIPTION
+    Converts an Azure AD Object ID to a SID.
+    Author: Oliver Kieselbach (oliverkieselbach.com)
+    The script is provided "AS IS" with no warranties.
+     
+    .PARAMETER ObjectID
+    The Object ID to convert
+    #>
+    
+        param([String] $ObjectId)
+    
+        $bytes = [Guid]::Parse($ObjectId).ToByteArray()
+        $array = New-Object 'UInt32[]' 4
+    
+        [Buffer]::BlockCopy($bytes, 0, $array, 0, 16)
+        $sid = "S-1-12-1-$array".Replace(' ', '-')
+    
+        return $sid
+    }
+    
+    $objectId = $objID
+    $sid = Convert-AzureAdObjectIdToSid -ObjectId $objectId
+    Write-Output $sid
+    
+    # Output:
+    
+    # S-1-12-1-1943430372-1249052806-2496021943-3034400218
