2638c07087002a03e2350778dbd491907463f525
Des Wass <des.wass@canaryit.com.au>
2025-05-17 09:00:45 +1000

    Removed secret

--- SecureAccess Automation/Get-RefreshToken-RestAPI.ps1
+++ SecureAccess Automation/Get-RefreshToken-RestAPI.ps1
@@ -1,10 +1,6 @@
 # Define variables
 $appId = '27ae0072-8bd0-4a12-aead-7e2a83ea5ae9'
-<<<<<<< HEAD
 $appSecret = 'SECRET'
-=======
-$appSecret = 'jte8Q~HodYn35H4lKi3P2pVrp3c3.SgIRHioLdov'
->>>>>>> 82a7a5a (Updates.)
 $tenantId = 'XXXX'
 $scope = 'https://api.partnercenter.microsoft.com/.default'
 $redirectUri = 'http://localhost:8400' # This should match the redirect URI registered in your app
